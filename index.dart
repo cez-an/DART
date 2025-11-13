89d2d01bd23ae8484884b2bb941fae89d445c4c2
Cez-an <cezan.git@gmail.com>
2025-11-13 11:16:45 +0530

    I’ve started studying Dart — a hands-on journey into understanding its core syntax, functions, classes, and object-oriented foundations from the ground up.

---
+++ index.dart
@@ -0,0 +1,57 @@
+void main(){
+  
+//   String classname = 'cezanne';
+//   print(classname);
+//   int studentCount = 25;
+//   print(studentCount);
+  
+//   String a = '10';
+//   String b = '11';
+  
+//   print(a+b);
+//   double avg = 36.6644;
+//   print(avg);
+  
+//   num avl = 1092;
+//   print(avl);
+//   num aavl = 12.0992;
+//   bool gat = true;
+//   print(gat);
+//   var nesw1 = 'cezanne';
+//   new1 = 100;
+  
+//   dynamic new1 = 'cezanne';
+//   new1 = 1000;
+//   print(new1);
+  
+//   int neww = 1221;
+  
+//   print('cezanne is $neww-23');
+  
+//   String new1 = "        cezanne is   a `hhhhh`   boy ";
+  
+//   print(new1);
+  
+//   List<dynamic> new11 = [12,12,'cdeds','232dwddcs',12,12,12,12,1212];
+  
+//   print('crzzadaaf');
+  
+  
+//   Map<String,dynamic> cez = {
+//     "name":"900asasasadad",
+//      'key':100,
+//   };
+//   print(cez);
+  
+
+  Map <String,dynamic> cez ={
+    "name":'cezanne',
+    "address":'baharathi bhavan, puthenkulam p.o, paravur, kollam,',
+    "mob":8478597964
+    
+  };
+  
+  print(cez);
+  
+}
+
